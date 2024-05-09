20844736b92aee9ff42653a89a5e143a35dc993e
Cyclopropinon <cyclopropinon@realprogrammers.org>
2024-05-09 22:50:43 +0200

    Bitte geben Sie eine Commit-Beschreibung für Ihre Änderungen ein. Zeilen, die mit '#' beginnen, werden ignoriert, und eine leere Beschreibung bricht den Commit ab.
    Auf Branch master
    Ihr Branch ist auf demselben Stand wie 'origin/master'.
    #
    Zum Commit vorgemerkte Änderungen:

---
+++ test.rs
@@ -0,0 +1,48 @@
+extern crate foo;
+extern crate bar;
+
+use foo::{self, quix};
+use bar::car::*;
+use bar;
+
+fn main() {
+    println!("Hello {}", "World");
+
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+    panic!("Goodbye")
+}
